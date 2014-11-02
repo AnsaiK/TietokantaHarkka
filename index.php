cd6d9ba8551c7bc36e5fcac151180686b2f7e207
AnssiK <anssi.kienanen@cs.helsinki.fi>
2014-11-02 14:11:11 +0200

    eka viikko

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
     <?php 
+          $title = "Tso ._. ha"; 
+
       $tervehdys = "he ._. lo"; 
     ?><!DOCTYPE HTML>
     <html>
-    <head><title><?php echo $tervehdys; ?></title></head>
+    <head><title><?php echo $title; ?></title></head>
     <body>
       <h1><?php echo $tervehdys; ?></h1>
     </body>
